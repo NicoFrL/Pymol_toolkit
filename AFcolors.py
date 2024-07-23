0bbd66d1485fce81cdef22d0badf2944b64b6382
Nicolas-Frédéric Lipp, PhD <40916571+NicoFrL@users.noreply.github.com>
2024-07-22 23:20:54 -0700

    Create AFcolors.py

---
+++ AFcolors.py
@@ -0,0 +1,34 @@
+from pymol import cmd
+
+
+def AFcolors(selection="all"):
+
+    """
+    AUTHOR
+    Christian Balbin 
+    
+    ADAPTATION
+    Nicolas-Frederic Lipp
+    
+    DESCRIPTION
+    Colors Alphafold structures by pLDDT
+    An improved version of the original to prevent the exact values of 90, 70, and 50 from not being recolored.
+    Ranges were checked on AFDB to ensure that 90, 70, and 50 match their color scheme, despite the nonsensical model confidence legend on the AFDB website.
+
+    USAGE
+    AFcolors sele
+
+    PARAMETERS
+
+    sele (string)
+    The name of the selection/object to color by pLDDT. Default: all
+    """
+
+    cmd.color("0x126DFF", f"({selection}) and (b > 90 or b = 90)")
+    cmd.color("0x0ECFF1", f"({selection}) and ((b < 90 and b > 70) or b = 70)")
+    cmd.color("0xF6ED12", f"({selection}) and ((b < 70 and b > 50) or b = 50)")
+    cmd.color("0xEE831D", f"({selection}) and b < 50")
+
+
+cmd.extend("AFcolors", AFcolors)
+cmd.auto_arg[0]["AFcolors"] = [cmd.object_sc, "object", ""]
