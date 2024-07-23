74a63234a5d65bed82ae3e7df3e161fa6575931a
Nicolas-Frédéric Lipp, PhD <40916571+NicoFrL@users.noreply.github.com>
2024-07-23 00:11:37 -0700

    Update AFcolors.py

--- AFcolors.py
+++ AFcolors.py
@@ -12,10 +12,11 @@ def AFcolors(selection="all"):
     
     DESCRIPTION
     Colors Alphafold structures by pLDDT
-    An improved version of the original to prevent the exact values of 90, 70, and 50 from not being recolored.
-    Ranges were checked on AFDB to ensure that 90, 70, and 50 match their color scheme, despite the nonsensical model confidence legend on the AFDB website.
+    An improved version of the original
+    Prevent exact values of 90, 70, and 50 from not being recolored.
+    Ranges were checked on AFDB to ensure that 90, 70, and 50 match their color scheme, despite the nonsensical model confidence legend available on AFDB website.
     The colors now match exactly AFDB color scheme
-
+    Replace 'coloraf' by 'AFcolors'
 
     USAGE
     AFcolors sele
@@ -26,10 +27,10 @@ def AFcolors(selection="all"):
     The name of the selection/object to color by pLDDT. Default: all
     """
 
-    cmd.color("0x126DFF", f"({selection}) and (b > 90 or b = 90)")
-    cmd.color("0x0ECFF1", f"({selection}) and ((b < 90 and b > 70) or b = 70)")
-    cmd.color("0xF6ED12", f"({selection}) and ((b < 70 and b > 50) or b = 50)")
-    cmd.color("0xEE831D", f"({selection}) and b < 50")
+    cmd.color("0x0053D6", f"({selection}) and (b > 90 or b = 90)")
+    cmd.color("0x64CBF3", f"({selection}) and ((b < 90 and b > 70) or b = 70)")
+    cmd.color("0xFFDB13", f"({selection}) and ((b < 70 and b > 50) or b = 50)")
+    cmd.color("0xFE7D45", f"({selection}) and b < 50")
 
 
 cmd.extend("AFcolors", AFcolors)
